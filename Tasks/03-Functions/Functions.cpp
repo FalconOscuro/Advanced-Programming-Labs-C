f1ce3cb8ba9646e3cea98973fc4234949051284f
Peter Garrod <p.jlgarrod@gmail.com>
2023-04-05 14:47:39 +0100

    Restructured directory

--- Tasks/03-Functions/Functions.cpp
+++ Tasks/03-Functions/Functions.cpp
@@ -9,12 +9,18 @@ int mymax(int a, int b)
 		return b;
 }
 
+void MyFunc(int a, bool c, char d)
+{
+	return;
+}
+
 int main(int, char**) {
 
 	int a = 10;
 	int b = 20;
 
 	int max = mymax(a, b);
+	MyFunc(2, false, 'c');
 
 	cout << "a=" << a << ", b=" << b << endl;
 	cout << "max=" << max << endl;
